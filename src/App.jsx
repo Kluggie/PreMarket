16057f7337f2347408b85b30d14a0650ee5537e8
Greg Klugman <gregoryklugman@gmail.com>
2026-02-08 23:43:46 +1100

    Revert "feat: allow editing saved proposal info (step 3)"

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ import DirectoryOrgDetail from '@/pages/DirectoryOrgDetail';
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
 const MainPage = mainPageKey ? Pages[mainPageKey] : <></>;
-const EditProposalPage = Pages.CreateProposalWithDrafts || Pages.CreateProposal;
 
 const LayoutWrapper = ({ children, currentPageName }) => Layout ?
   <Layout currentPageName={currentPageName}>{children}</Layout>
@@ -44,14 +43,6 @@ const PublicRoutes = () => {
         element={
           <LayoutWrapper currentPageName="Directory">
             <DirectoryOrgDetail />
-          </LayoutWrapper>
-        }
-      />
-      <Route
-        path="/proposals/:proposalId/edit"
-        element={
-          <LayoutWrapper currentPageName="CreateProposalWithDrafts">
-            <EditProposalPage />
           </LayoutWrapper>
         }
       />
